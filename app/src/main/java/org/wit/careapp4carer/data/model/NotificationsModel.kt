d5d7e6025969b228f127cc81f9c24a936162ce32
LAPTOP-QQULPCJ7\edyta <edyta.sleziak@gmail.com>
2020-03-01 21:54:35 +0000

    Notification layouts added

--- app/src/main/java/org/wit/careapp4carer/data/model/NotificationsModel.kt
+++ app/src/main/java/org/wit/careapp4carer/data/model/NotificationsModel.kt
@@ -3,9 +3,8 @@ package org.wit.careapp4carer.models
 import java.time.LocalDateTime
 
 data class NotificationsModel(
-    var id: Long = 0,
+    var id: String = "",
     var notification: String = "",
     var displayTime: LocalDateTime,
-    var completedTime: LocalDateTime,
-    var userId: Long = 0
+    var completedTime: LocalDateTime
 )
